763039f21e38b65a45dd45a988a3d05da8986f5f
Barbara2202 <82990188+Barbara2202@users.noreply.github.com>
2021-04-23 14:46:43 -0400

    Update 210423-familia-integrantes-v01.js

--- 210423-familia-integrantes-v01.js
+++ 210423-familia-integrantes-v01.js
@@ -2,133 +2,134 @@ class Cl_mIntegrante
 {
     constructor(iM = 0, gM = 0)
     {
-        this.ingresoMensual = 0;
-        this.gastoFijoMensual = 0;
-    };
+        this.ingresoMensual = iM
+        this.gastoFijoMensual = gM
+    }
     get ingresoMensual()
     {
-        return this._ingresoMensual;
-    };
+        return this._ingresoMensual
+    }
     set ingresoMensual(iM)
     {
-        this._ingresoMensual = iM;
-    };
+        this._ingresoMensual = parseFloat(iM)
+    }
     get gastoFijoMensual()
     {
-        return this._gastoFijoMensual;
-    };
+        return this._gastoFijoMensual
+    }
     set gastoFijoMensual(gM)
     {
-        this._gastoFijoMensual = gM;
-    };
+        this._gastoFijoMensual = parseFloat(gM)
+    }
     AporteMensual()
     {
-        return (this.ingresoMensual - this.gastoFijoMensual);
-    };
-};
+        return this.ingresoMensual - this.gastoFijoMensual
+    }
+}
 
 class Cl_mFamilia
 {
     constructor()
     {
-        this.montoTotalD = 0;
-    };
+        this.montoTotalD = 0
+    }
     get montoTotalD()
     {
-        return this._montoTotalD;
-    };
+        return this._montoTotalD
+    }
     set montoTotalD(mD)
     {
-        this._montoTotalD = parseFloat(mD);
-    };
+        this._montoTotalD = parseFloat(mD)
+    }
     ProcesarIntegrante(i)
     {
-        this.montoTotalD += i.AporteMensual();
-    };
-};
+        this.montoTotalD += i.AporteMensual()
+    }
+}
 
 class Cl_vFamilia
 {
     reportar(montoTotalD)
     {
-        consola_salida.innerHTML += `<br>MONTO TOTAL DISPONIBLE EN LA FAMILIA: ${montoTotalD}<br>`;
-    };    
-};
+        consola_salida.innerHTML += `<br>MONTO TOTAL DISPONIBLE EN LA FAMILIA: ${montoTotalD}<br>`
+    } 
+}
 
 class Cl_vIntegrante
 {
     reportar(i)
     {
         consola_salida.innerHTML +=
-            `${String(i.ingresoMensual).padEnd(10, '.')}` +
-            `${String(i.gastoFijoMensual.padStar(10, '.'))}` +
-            `${String(i.AporteMensual.padStar(10, '.'))}<br>`
-    };
-};
+            `${String(i.ingresoMensual).padStart(10, '.')}` +
+            `${String(i.gastoFijoMensual).padStart(20, '.')}` +
+            `${String(i.AporteMensual()).padStart(20, '.')}<br>`
+    }
+}
 
 class Cl_controler
 {
     constructor()
     {
-        this.mIntegrante = new Cl_mIntegrante();
-        this.mFamilia = new Cl_mFamilia();
-        this.vFamilia = new Cl_vFamilia();
-        this.vIntegrante = new Cl_vIntegrante();
-    };
+        this.mIntegrante = new Cl_mIntegrante()
+        this.mFamilia = new Cl_mFamilia()
+        this.vFamilia = new Cl_vFamilia()
+        this.vIntegrante = new Cl_vIntegrante()
+    }
     get mIntegrante()
     {
-        return this._mIntegrante;
-    };
+        return this._mIntegrante
+    }
     set mIntegrante(i)
     {
-        this._mIntegrante = i;
-    };
+        this._mIntegrante = i
+    }
     get mFamilia()
     {
-        return this._mFamilia;
-    };
+        return this._mFamilia
+    }
     set mFamilia(f)
     {
-        this._mFamilia = f;
-    };
+        this._mFamilia = f
+    }
     get vFamilia()
     {
-        return this._vFamilia;
-    };
+        return this._vFamilia
+    }
     set vFamilia(f)
     {
-        this._vFamilia = f;
-    };
+        this._vFamilia = f
+    }
     get vIntegrante()
     {
-        return this._vIntegrante;
-    };
+        return this._vIntegrante
+    }
     set vIntegrante(i)
     {
-        this._vIntegrante = i;
-    };
+        this._vIntegrante = i
+    }
     procesar1()
     {
         consola_salida.innerHTML = 
-            'Ingreso Mensual        Gasto Fijo Mensual        Aporte Mensual<br>' +
-            '===============================================================<br>'
-        let integrante1 = new Cl_mVendedor(1000, 500, 500);
-        let integrante2 = new Cl_mVendedor(750, 500, 250);
-        let integrante3 = new Cl_mVendedor(250, 80, 170);
-        let integrante4 = new Cl_mVendedor(50, 20, 30);
-        this.mFamilia.ProcesarIntegrante(integrante1);
-        this.vIntegrante.reportar(integrante1);
-        this.mFamilia.ProcesarIntegrante(integrante2);
-        this.vIntegrante.reportar(integrante2);
-        this.mFamilia.ProcesarIntegrante(integrante3);
-        this.vIntegrante.reportar(integrante3);
-        this.mFamilia.ProcesarIntegrante(integrante4);
-        this.vIntegrante.ProcesarIntegrante(integrante4);
-        this.vFamilia.reportar(this.mFamilia.montoTotalD);
+            '---------------------------EJEMPLO---------------------------<br>' +
+            '- Ingreso Mensual --- Gasto Fijo Mensual --- Aporte Mensual -<br>' +
+            '_____________________________________________________________<br>'
+        let integrante1 = new Cl_mIntegrante(1000, 500)
+        let integrante2 = new Cl_mIntegrante(750, 500)
+        let integrante3 = new Cl_mIntegrante(250, 80)
+        let integrante4 = new Cl_mIntegrante(50, 20)
+        this.mFamilia.ProcesarIntegrante(integrante1)
+        this.vIntegrante.reportar(integrante1)
+        this.mFamilia.ProcesarIntegrante(integrante2)
+        this.vIntegrante.reportar(integrante2)
+        this.mFamilia.ProcesarIntegrante(integrante3)
+        this.vIntegrante.reportar(integrante3)
+        this.mFamilia.ProcesarIntegrante(integrante4)
+        this.vIntegrante.reportar(integrante4)
+        this.vFamilia.reportar(this.mFamilia.montoTotalD)
         consola_salida.innerHTML +=
-            '===============================================================<br><br>'
-        this.procesar2(); 
-    };
+            '_____________________________________________________________<br><br>'
+        this.procesar2()
+    }
     procesar2()
     {
         let procesar = confirm('¿Procesar datos manualmente?'),
@@ -138,21 +139,22 @@ class Cl_controler
         {
             this.mFamilia.montoTotalD = 0
             consola_salida.innerHTML +=
-                'Ingreso Mensual        Gasto Fijo Mensual        Aporte Mensual<br>' +
-                '===============================================================<br>'
+                '----------------------------MANUAL---------------------------<br>' +
+                '- Ingreso Mensual --- Gasto Fijo Mensual --- Aporte Mensual -<br>' +
+                '_____________________________________________________________<br>'
             while (procesar)
             {
-                this.mIntegrante.ingresoMensual = prompt('Indique su ingreso mensual');
-                this.mIntegrante.gastoFijoMensual = prompt('Indique su gasto fijo mensual');
-                this.mFamilia.ProcesarIntegrante(this.mIntegrante);
-                this.vIntegrante.reportar(this.mIntegrante);
-                procesar = confirm('¿Hay otro integrante?');
-            };
-            this.vFamilia.reportar(this.mFamilia.montoTotalD);
+                this.mIntegrante.ingresoMensual = prompt('Indique su ingreso mensual')
+                this.mIntegrante.gastoFijoMensual = prompt('Indique su gasto fijo mensual')
+                this.mFamilia.ProcesarIntegrante(this.mIntegrante)
+                this.vIntegrante.reportar(this.mIntegrante)
+                procesar = confirm('¿Hay otro integrante?')
+            }
+            this.vFamilia.reportar(this.mFamilia.montoTotalD)
             consola_salida.innerHTML +=
-                '===============================================================<br>'
-        };
-    };
-};
+                '_____________________________________________________________<br>'
+        }
+    }
+}
 
-var controler = new Cl_controler();
+var controler = new Cl_controler()
